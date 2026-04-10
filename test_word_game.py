4aff3e7bda16e9d522ede47b9542353e143f7a32
j246512 <J246512@tafe.wa.edu.au>
2026-04-10 13:44:54 +0800

    myfeatures

---
+++ test_word_game.py
@@ -0,0 +1,14 @@
+def simple_math():
+    result = 2 + 2
+
+
+def is_letter_5(apples):
+    return len(apples)
+
+
+def test_simple_math():
+    assert 2 + 2 == 4
+
+
+def test_is_letter_5():
+    assert "apples"(len) == 5
